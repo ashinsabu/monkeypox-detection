887cfc09b83f76238db96f4a64118bae10561fa6
ashinsabu <ashin.sabu3@gmail.com>
2023-04-07 02:57:46 +0530

    Fixed a bug in hosting where homepage was not rendering correctly

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import MobileNet from './pages/MobileNet';
 import Home from './pages/Home';
 import Trymodels from './pages/Trymodel'
 
 const App = () => {
   return (
-      <HashRouter>
+      <BrowserRouter>
             <Routes>
+                <Route exact path="/" element = {<Home/> } />
                 <Route exact path="/" element = {<Home/> } />
                 <Route exact path="/try" element = {<Trymodels/> } />
                 <Route exact path="/try/mobilenet" element = {<MobileNet/>} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
   );
 }
 
