e8efc2fb7364e01d210678cdffb00bf685454bda
ashinsabu <ashin.sabu3@gmail.com>
2023-04-07 02:04:32 +0530

    Initialized firebase and added firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
 import MobileNet from './pages/MobileNet';
 import Home from './pages/Home';
 import Trymodels from './pages/Trymodel'
+
 const App = () => {
   return (
-      <BrowserRouter>
+      <HashRouter>
             <Routes>
                 <Route exact path="/" element = {<Home/> } />
                 <Route exact path="/try" element = {<Trymodels/> } />
                 <Route exact path="/try/mobilenet" element = {<MobileNet/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEpfdBrvNBn2ulFH95VV436bYZL3a-OjM",
+    authDomain: "monkeypox-detection-2bccd.firebaseapp.com",
+    projectId: "monkeypox-detection-2bccd",
+    storageBucket: "monkeypox-detection-2bccd.appspot.com",
+    messagingSenderId: "205269813004",
+    appId: "1:205269813004:web:c03e9af702937dd7b92b29",
+    measurementId: "G-W552DHHGK9"
+  };
+const app = firebase.initializeApp(firebaseConfig);
+firebase.getAnalytics(app);
+
+export default firebase;
+// to add features https://stackoverflow.com/questions/48492047/where-do-i-initialize-firebase-app-in-react-application
+// https://stackoverflow.com/questions/51011486/initializing-firebase-cli-features-in-an-existing-firebase-project-directory
+// deploy command firebase deploy --only hosting:monkeypoxdetection
