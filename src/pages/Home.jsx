0d71b140c6de678b210b172f331177d4c49d5543
ashinsabu <ashin.sabu3@gmail.com>
2023-04-14 05:58:56 +0530

    Added more content placeholders for the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,33 @@ import './Home.css';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
+import neuralNetImg from "../assets/pngegg.png"
 const Home = () => {
     return (
         <div>
             <NavBar curPage = {0}/>
-            <div className="hero">
-                <h2>Highly accurate, </h2>
-                <h1>State of the art </h1>
-                <h2>Monkeypox Detection Models</h2>
-                <div className='hero-bottom-buttons'>
-                    <Link to="/research" id='viewResearch'><p>View research</p></Link>
-                    <Link to="/try" id='viewPlayground'><p>Models Playground</p></Link>
+            <div className="main-container">
+                <div className="hero">
+                    <h2>Highly accurate, </h2>
+                    <h1>State of the art </h1>
+                    <h2>Monkeypox Detection Models</h2>
+                    <div className='hero-bottom-buttons'>
+                        <Link to="/research" className='box-with-white-hover-bg'><p>View research</p></Link>
+                        <Link to="/try" id='viewPlayground'><p>Models Playground </p></Link>
+                    </div>
+                </div>
+                <div className='home-content'>
+                    <h1>The models</h1>
+                    <div className="info-container">
+                        <img src={neuralNetImg} alt="ResNet"></img>
+                        <div>
+                            <h1>MobileNet V3</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident doloribus nesciunt accusantium nisi sunt aperiam perferendis sed ad voluptates quidem incidunt iure iste, sapiente accusamus ducimus nostrum ex illo?</p>
+                            <Link to="/try/mobilenet" className='box-with-white-hover-bg'><p>Try MobileNet {">>"}</p></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <p style={{textAlign: "center", marginTop: 128}}>HomePage</p>
             <Footer/>
         </div>
     )
