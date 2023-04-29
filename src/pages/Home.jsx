e29868f146dac0271411721035e794b6717f014c
ashinsabu <ashin.sabu3@gmail.com>
2023-04-30 03:07:31 +0530

    Minor UI responsiveness fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
                         <img src={neuralnetImg2} alt="MobileNet"></img>
                         <div>
                             <h1>MobileNet</h1>
-                            <p className='info-text'>MobileNet is a convolutional neural network (CNN) model that is designed for efficient image classification on mobile devices. It uses depthwise separable convolutions to reduce computation and model size. The model consists of a series of convolutional layers followed by depthwise separable convolutional layers, which separate spatial and channel-wise operations. </p>
+                            <p className='info-text'>MobileNet V2 is a convolutional neural network (CNN) model that is designed for efficient image classification on mobile devices. It uses depthwise separable convolutions to reduce computation and model size. The model consists of a series of convolutional layers followed by depthwise separable convolutional layers, which separate spatial and channel-wise operations. </p>
                             <p className='info-text'>To customize MobileNet, additional layers can be added on top, such as fully connected layers for classification, or additional convolutional layers for feature extraction. These added layers can provide further customization and adaptation to specific tasks or datasets, making MobileNet a versatile option for mobile image recognition applications.</p>
                             <p className='info-text'>The implementation used here is a custom version. Read more about it in <Link className='within-text-link' to ="/research">the research.</Link></p>
                             <Link to="/try/mobilenet" className='box-with-white-hover-bg'><p>Try MobileNet {">>"}</p></Link>
@@ -36,11 +36,12 @@ const Home = () => {
                         <img src={neuralnetImg3} alt="ResNet"></img>
                         <div>
                             <h1>ResNet50</h1>
-                            <p className='info-text'>ResNet50V2 is a variant of the ResNet (Residual Network) model, which is a popular convolutional neural network (CNN) architecture for image classification. It was introduced as an improvement over the original ResNet model to further enhance its performance.</p>
+                            <p className='info-text'>ResNet50 V2 is a variant of the ResNet (Residual Network) model, which is a popular convolutional neural network (CNN) architecture for image classification. It was introduced as an improvement over the original ResNet model to further enhance its performance.</p>
                             <p className='info-text'>The ResNet architecture is known for using residual blocks, which address the vanishing gradient problem that can occur in deep neural networks. The vanishing gradient problem refers to the issue of gradients becoming too small during backpropagation, which can hinder the learning process. Residual blocks introduce skip connections, also known as shortcut connections, that allow for the direct flow of information from earlier layers to later layers. This allows the network to bypass unnecessary layers and enables training of deeper networks with improved gradient flow and reduced degradation in performance as the network gets deeper.</p>
                             <p className='info-text'>ResNet50V2 specifically consists of 50 layers, including convolutional layers, batch normalization layers, activation functions (such as Rectified Linear Units or ReLUs), global average pooling layers, and fully connected layers. It follows the "pre-activation" design, where batch normalization and activation functions are applied before the convolutional layers, allowing for better convergence during training.</p>
-                            <span style={{color: "#ffffffa1"}}> Model not hosted yet due to size</span>
-                        </div>
+                            <p className='info-text'>The deployed model is large and may take a while to load. Read more about it in <Link className='within-text-link' to ="/research">the research.</Link></p>
+
+                            <Link to="/try/resnet" className='box-with-white-hover-bg'><p>Try ResNet50 {">>"}</p></Link>                        </div>
                     </div>
                     <div className="info-container">
                         <img src={neuralNetImg} alt="Xception"></img>
