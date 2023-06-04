49d7a6e06fc9d7778e05a683271fdf5b866a868e
ashinsabu <ashin.sabu3@gmail.com>
2023-06-04 22:42:04 +0530

    Added loading indicator inside model container

--- src/pages/MobileNet.jsx
+++ src/pages/MobileNet.jsx
@@ -113,11 +113,12 @@ function Mobilenet() {
           <div className='modelContainer mobilenetbg'>
 
             {modelLoading? 
-            <div>
-              <p style={{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '200px', padding: '16px', boxSizing:'border-box', flexDirection: 'column',gap:'32px'}}>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+              <p style={{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '128px', padding: '16px', boxSizing:'border-box', flexDirection: 'column',gap:'32px'}}>
                 <span>Downloading and initializing MobileNet from Cloud Resource...</span>
                 <span style={{backgroundColor: '#00000046', padding:'4px'}}>Progress: {(Math.round(modelLoadProgress*100 * 100) / 100).toFixed(2)}%</span></p>
-                <span></span>
+                <span>Download: {modelLoadProgress<0.5?<span>{(Math.round((modelLoadProgress/0.5)*100 * 100) / 100).toFixed(2)}%</span>:<span>Complete</span>}</span>
+                {modelLoadProgress>=0.5?<span>Initialization: {(Math.round((modelLoadProgress-0.5)/0.5*100 * 100) / 100).toFixed(2)}%</span>:<></>}
             </div>
             : 
             <>
